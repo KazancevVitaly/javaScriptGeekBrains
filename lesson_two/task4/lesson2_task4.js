c9ce33fc2bfa50d8f99c500d203c9377ef72e9c7
Vitaly <kazancevvitallijj@gmail.com>
2021-03-26 12:55:35 +0800

    task8

--- lesson_two/task4/lesson2_task4.js
+++ lesson_two/task4/lesson2_task4.js
@@ -10,54 +10,40 @@ let a = 0;
 switch(a){
 	case 0:
 		console.log(a++);
-		break;
 	case 1:
 		console.log(a++);
-		break;
 	case 2:
 		console.log(a++);
-		break;
 	case 3:
 		console.log(a++);
-		break;
 	case 4:
 		console.log(a++);
-		break;
 	case 5:
 		console.log(a++);
-		break;
 	case 6:
 		console.log(a++);
-		break;
 	case 7:
 		console.log(a++);
-		break;
 	case 8:
 		console.log(a++);
-		break;
 	case 9:
 		console.log(a++);
-		break;
 	case 10:
 		console.log(a++);
-		break;
 	case 11:
 		console.log(a++);
-		break;
 	case 12:
 		console.log(a++);
-		break;
 	case 13:
 		console.log(a++);
-		break;
 	case 14:
 		console.log(a++);
-		break;
 	case 15:
 		console.log(a++);
 		break;
-	default
-		console.log('Необходимо значение от 0 до 15')
+	default:
+		console.log('Необходимо значение от 0 до 15');
+		break;
  }
  
  // Про постфиксный инкремент не догадался, поначалу собирал строку из чисел, исправил после урока.
