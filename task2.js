da9e03d21844edbbff2adcc849fc83290dc6b416
Vitaly <kazancevvitallijj@gmail.com>
2021-04-04 22:58:58 +0800

    Task2

--- task2.js
+++ task2.js
@@ -6,7 +6,7 @@
  ***************************************************************************************************************************/
  
  // 2.1. элементы массива, на основе которого была создана корзина, можно заменить
- // объектами product с присущеми этим оъектам свойствами name, price и прочие характеристки товара
+ // объектами product с присущими этим оъектам свойствами name, price и прочие характеристки товара
  
  // 2.2
  let product = {
@@ -23,19 +23,22 @@
 	 name: 'skates',
 	 price: 754,
  };
- 
- //console.log(product['name']);
 
  //2.3
  basket = {
+	 //список товаров
 	 listProduct: [],
+	 //метод добавления товаров в корзину
 	 listProductPush(prod){
 		 this.listProduct.push(prod);
 	 },
+	 //количество товаров в корзине
 	 ammountProduct(){
-		 this.listProduct.length;
+		 return this.listProduct.length;
 	 },
+	 //стоимость товаров в корзине
 	 basketPrice: 0,
+	 //метод подсчета стоимости товаров в корзине
 	 countBasketPrice() {
 		 for (i = 0; i < this.listProduct.length; i++){
 			 this.basketPrice = this.basketPrice + this.listProduct[i].price;
@@ -43,15 +46,26 @@
 	 },
  };
  
+ //проверяем список товаров в корзине --- список пуст
  console.log(basket.listProduct);
- console.log(basket.ammountProduct);
+ //проверяем количество товаров -- товаров ноль
+ console.log(basket.ammountProduct());
+ //считаем стоимость
+ basket.countBasketPrice();
+ // стоимость - ноль
  console.log(basket.basketPrice);
+ 
+ //добавляем товары в корзину
  basket.listProductPush(product);
  basket.listProductPush(product1);
  basket.listProductPush(product2);
+ //еще раз смотрим список -- список изменился
  console.log(basket.listProduct);
- console.log(basket.ammountProduct);
+ // количество товаров тепер равно 3
+ console.log(basket.ammountProduct());
+ // считаем стоимость
+ basket.countBasketPrice();
+ // стоимость равна 1204
  console.log(basket.basketPrice);
- console.log(basket.listProduct.length);
  
  
